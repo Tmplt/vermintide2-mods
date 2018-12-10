35927c13861df48477c087a4140c32ab2ef8e337
Tmplt <tmplt@dragons.rocks>
2018-12-11 00:49:24 +0100

    NoOverchargeDmgIndicator: properly set *_{data,localization}.lua
    Resolves #1

--- NoOverchargeDmgIndicator/scripts/mods/NoOverchargeDmgIndicator/NoOverchargeDmgIndicator_data.lua
+++ NoOverchargeDmgIndicator/scripts/mods/NoOverchargeDmgIndicator/NoOverchargeDmgIndicator_data.lua
@@ -2,18 +2,10 @@ local mod = get_mod("NoOverchargeDmgIndicator")
 
 -- Everything here is optional. You can remove unused parts.
 return {
-	name = "NoOverchargeDmgIndicator",                               -- Readable mod name
+	name = "No Overcharge Damage Indicator",        -- Readable mod name
 	description = mod:localize("mod_description"),  -- Mod description
 	is_togglable = true,                            -- If the mod can be enabled/disabled
 	is_mutator = false,                             -- If the mod is mutator
 	mutator_settings = {},                          -- Extra settings, if it's mutator
-	options_widgets = {                             -- Widget settings for the mod options menu
-		{
-			setting_name = "example_checkbox",
-			widget_type = "checkbox",
-			text = mod:localize("example_checkbox_option_name"),
-			tooltip = mod:localize("example_checkbox_option_tooltip"),
-			default_value = true
-		}
-	}
+	options_widgets = {}                            -- Widget settings for the mod options menu
 }
